bfe34574a9fae64eaf58fc8781d2f2e2b1309f26
torufa <torufa3@gmail.com>
2025-12-17 21:49:59 +0600

    theme changing

---
+++ script.js
@@ -0,0 +1,33 @@
+let body = document.querySelector('body');
+let btn = document.querySelector('button');
+
+function theme() {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }else{
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+}
+theme();
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(){
+    theme();
+});
+
+
+body.classList.add(localStorage.getItem('theme'));
+
+btn.addEventListener("click", function(){
+    if(body.classList.contains('dark')){
+        body.classList.remove('dark');
+        body.classList.add('light');
+
+        localStorage.setItem('theme', "light");
+    }else{
+        body.classList.remove('light');
+        body.classList.add('dark');
+
+        localStorage.setItem('theme', "dark");
+    }
+})
